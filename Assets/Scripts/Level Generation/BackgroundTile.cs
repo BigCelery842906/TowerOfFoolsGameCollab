94ddc136514601c7be4421fe9ef664c5554197b9
Indapoopski <163399129+Indapoopski@users.noreply.github.com>
2026-03-02 16:18:02 +0000

    Created Stubs for Level Gen

---
+++ Assets/Scripts/Level Generation/BackgroundTile.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class BackgroundTile : MonoBehaviour
+{
+
+    Sprite m_texture;
+    Vector2 m_dimensions;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Level Generation/GenerationManager.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerationManager : MonoBehaviour
+{
+
+    List<BackgroundTile> m_backgroundTilePool;
+    BackgroundTile m_lastTilePlaced;
+    PlatformGenerator m_platformGenerator;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+    void GenerateNewTile()
+    {
+
+    }
+    void GeneratePlatform()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
