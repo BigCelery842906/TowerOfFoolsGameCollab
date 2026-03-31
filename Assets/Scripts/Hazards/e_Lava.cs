06112d2d77a2d0c4c2d8d42e7539d07a594421fe
Donk-e <tywood722@gmail.com>
2026-03-31 13:58:39 +0100

    shield works with lava xx

--- Assets/Scripts/Hazards/e_Lava.cs
+++ Assets/Scripts/Hazards/e_Lava.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 // Written by Connor Saysell
@@ -31,8 +32,26 @@ public class e_Lava : MonoBehaviour
         if (other.gameObject.tag == "Player")
         {
             // "If you finger the lava, you're dead" - Connor Holt 2026
-            Debug.Log("Lava Fingered", other.gameObject);
+            Debug.Log("Lava Fingered" + other.gameObject.name);
             
+            //player touched lava so we should see if they have a shield  //Tyler xx
+            p_PlayerPickupManager playerPickupMan = other.gameObject.GetComponentInParent<p_PlayerPickupManager>();
+            if (playerPickupMan != null)
+            {
+                if (playerPickupMan.GetPlayerShield())
+                {
+                    //player has shield, they should be moved up?
+
+                    Vector3 tempTransfrom = playerPickupMan.gameObject.transform.position; //for readability sake 
+                    float offset = playerPickupMan.GetShieldLavaOffset();
+                    playerPickupMan.gameObject.transform.position = new Vector3(tempTransfrom.x,tempTransfrom.y + offset,tempTransfrom.z);
+
+                    //players been moved now remove their shield
+                    playerPickupMan.SetPlayerShield(false, 0f);
+
+                    return;
+                }
+            }
             
             //The parent of the gameobject has the player ID within the tag, use this to get the player ID
             int playerID = -1; //default to -1 in case of error
@@ -43,6 +62,7 @@ public class e_Lava : MonoBehaviour
                 Debug.LogError("Player found but ID not returned", other.gameObject);
                 return;
             }
+            e_GameEvents.instance.PlayerHealthUpdate(false, 100, playerID);
             e_GameEvents.instance.PlayerHealthUpdate(false, 100, playerID);
         }
     }
