9eb4552e27fed536318372d07fc00fb4a5f23c81
Donk-e <tywood722@gmail.com>
2026-04-02 23:38:25 +0100

    hate this bug sm >:c

--- Assets/Scripts/Pick-ups/Mobility/pu_Swap.cs
+++ Assets/Scripts/Pick-ups/Mobility/pu_Swap.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class pu_Swap : BasePickup
 {
+    public Vector3 tempPlayerTrans;
+    public Vector3 tempOtherPlayerTrans;
+
+
     protected override void PickupEffect()
     {
         m_triggeredPlayer.SetIsInteractablePickup(true,this);
@@ -19,15 +23,18 @@ public class pu_Swap : BasePickup
         Debug.Log("player = " + m_triggeredPlayer.gameObject.name + "  other : " + m_otherPlayer.name);
 
         //Grab the positions of each player
-        Vector3 tempPlayerTrans = m_triggeredPlayer.transform.position;
-        Vector3 tempOtherPlayerTrans = m_otherPlayer.transform.position;
+        //Vector3 tempPlayerTrans = m_triggeredPlayer.transform.position;
+        //Vector3 tempOtherPlayerTrans = m_otherPlayer.transform.position;        
+
+        tempPlayerTrans = m_triggeredPlayer.transform.position;
+        tempOtherPlayerTrans = m_otherPlayer.transform.position;
 
         Debug.Log(tempPlayerTrans + "player");
         Debug.Log(tempOtherPlayerTrans + "other player");
 
         //Swap their positions
-        m_triggeredPlayer.transform.position = tempOtherPlayerTrans;
         m_otherPlayer.transform.position = tempPlayerTrans;
+        m_triggeredPlayer.transform.position = tempOtherPlayerTrans;
 
         PickupUsed();
     }
