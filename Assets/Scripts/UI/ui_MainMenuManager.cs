f660c4242fefa0792a138e9a109aa0923b2da1d1
xP9nda <93463780+xP9nda@users.noreply.github.com>
2026-04-09 11:35:47 +0100

    ui main menu manager refactor to use base class

--- Assets/Scripts/UI/ui_MainMenuManager.cs
+++ Assets/Scripts/UI/ui_MainMenuManager.cs
@@ -1,12 +1,10 @@
+using System.Collections.Generic;
+using UI;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class ui_MainMenuManager : MonoBehaviour
+public class ui_MainMenuManager : ui_BaseMenuManager
 {
-    [Header("UI Settings")]
-    [Tooltip("The UI Document for the Main Menu.")]
-    [SerializeField] private UIDocument m_uiMainMenuDocument;
-
     [Header("Gameplay Level Settings")]
     [SerializeField] private bool m_useSceneBuildIndex;
     
@@ -16,50 +14,11 @@ public class ui_MainMenuManager : MonoBehaviour
     [Tooltip("This value will only be taken into account if 'Use Scene Build Index' is false.")]
     [SerializeField] private string  m_sceneName;
 
-    private Button m_playButton;
-    private Button m_scoreboardButton;
-    private Button m_quitButton;
-    
-    void Awake()
+    protected override void InitialiseMenuManager()
     {
-        // Check if the UI Document is valid
-        if (m_uiMainMenuDocument == null)
-        {
-            throw new UnityException("ui_MainMenuDocument is null");
-        }
-        
-        // Query the buttons from the UI document, check validity, and set click callbacks
-        m_playButton = m_uiMainMenuDocument.rootVisualElement.Q<Button>("play-btn");
-        m_scoreboardButton =  m_uiMainMenuDocument.rootVisualElement.Q<Button>("scoreboard-btn");
-        m_quitButton = m_uiMainMenuDocument.rootVisualElement.Q<Button>("quit-btn");
-
-        if (m_playButton == null || m_scoreboardButton == null || m_quitButton == null)
-        {
-            throw new UnityException("ui_MainMenuDocument play or quit button is invalid");
-        }
-
-        m_playButton.clicked += HandleButtonClicked_Play;
-        m_scoreboardButton.clicked += HandleButtonClicked_Scoreboard;
-        m_quitButton.clicked += HandleButtonClicked_Quit;
-    }
-
-    private void OnDestroy()
-    {
-        // Just to be safe, unbind the callbacks when the menu is destroyed
-        if (m_playButton != null)
-        {
-            m_playButton.clicked -= HandleButtonClicked_Play;
-        }
-
-        if (m_scoreboardButton != null)
-        {
-            m_scoreboardButton.clicked -= HandleButtonClicked_Scoreboard;
-        }
-        
-        if (m_quitButton != null)
-        {
-            m_quitButton.clicked -= HandleButtonClicked_Quit;
-        }
+        BindButton("play-btn", HandleButtonClicked_Play);
+        BindButton("play-scoreboard", HandleButtonClicked_Scoreboard);
+        BindButton("play-quit", HandleButtonClicked_Quit);
     }
 
     private void HandleButtonClicked_Play()
@@ -76,7 +35,8 @@ public class ui_MainMenuManager : MonoBehaviour
 
     private void HandleButtonClicked_Scoreboard()
     {
-        
+        // TODO: Scoreboard button implementation
+        Debug.Log("Scoreboard Button Clicked, implementation todo");
     }
 
     private void HandleButtonClicked_Quit()
