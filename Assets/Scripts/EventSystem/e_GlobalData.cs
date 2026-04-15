a0cc83db9036064d15c49e747ecbeff0b4c18df6
BigCelery842906 <84037295+BigCelery842906@users.noreply.github.com>
2026-04-16 00:16:51 +0100

    First draft of global data
    It is already attached to the event system, meaning once this gets merged, it will automatically be added.
    The camera uses it, and so does the proc gen. The lava _can_, but currently that is commented out. I need to speak to design and properly look to see if there is any benefit attaching the lava to it.

---
+++ Assets/Scripts/EventSystem/e_GlobalData.cs
@@ -0,0 +1,96 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+// Written by Connor Saysell
+public class e_GlobalData : MonoBehaviour
+{
+
+    public static e_GlobalData instance;
+    
+    [SerializeField] float m_WorldScale;
+
+    [SerializeField] private float m_PlayerScale;
+
+    [Tooltip("The players that you intend to track. It will auto populate on start, overriding anything put in here previously. It can be changed to not do this if needed.")]
+    [SerializeField] private List<GameObject> m_PlayersToTrack;
+
+    [SerializeField] private float m_TimeSpentInCurrentRoom;
+
+    [SerializeField] private float m_TimeSpentInGame;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        GameObject[] tempGO = GameObject.FindGameObjectsWithTag("Player");
+        // This would be the capsule component of the player, so I need to get the parent for the actual physical player
+        
+        m_PlayersToTrack = new List<GameObject>();
+        for (int i = 0; i < tempGO.Length; i++)
+        {
+            m_PlayersToTrack.Add(tempGO[i].transform.parent.gameObject);
+        }
+        
+        m_TimeSpentInCurrentRoom = 0;
+        m_TimeSpentInGame = 0;
+    }
+    
+    void Update()
+    {
+        m_TimeSpentInCurrentRoom += Time.deltaTime;
+        m_TimeSpentInGame += Time.deltaTime;
+    }
+
+    public float GetWorldScale()
+    {
+        return m_WorldScale;
+    }
+
+    public float GetPlayerScale()
+    {
+        return m_PlayerScale;
+    }
+
+    public Vector3 GetPlayerPosition(int playerNum)
+    {
+        if (playerNum is 0 or 1)
+        {
+            return m_PlayersToTrack[playerNum].transform.position;
+        }
+        else return Vector3.zero;
+    }
+
+    public GameObject GetPlayer(int playerNum)
+    {
+        if (playerNum is 0 or 1)
+        {
+            return m_PlayersToTrack[playerNum];
+        }
+        else return null;
+    }
+    
+    public void ResetRoomTimer()
+    {
+        m_TimeSpentInCurrentRoom = 0;
+    }
+    
+    public float GetCurrentTimeSpentInRoom()
+    {
+        return m_TimeSpentInCurrentRoom;
+    }
+
+    public float GetCurrentTimeSpentInGame()
+    {
+        return m_TimeSpentInGame;
+    }
+    
+}
