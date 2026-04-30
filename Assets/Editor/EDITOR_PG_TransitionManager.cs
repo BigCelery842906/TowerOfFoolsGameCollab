d3d538a56e246eee64fc4dad1ecd4f7106b7273a
Indapoopski <163399129+Indapoopski@users.noreply.github.com>
2026-04-30 11:43:30 +0100

    added button to generate new room in transition manager

---
+++ Assets/Editor/EDITOR_PG_TransitionManager.cs
@@ -0,0 +1,20 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(PG_TransitionManager), true)]
+public class EDITOR_PG_TransitionManager : Editor
+{
+    void OnEnable()
+    {
+
+    }
+    public override void OnInspectorGUI()
+    {
+        PG_TransitionManager manager = target as PG_TransitionManager;
+        DrawDefaultInspector();
+        if(GUILayout.Button("Generate Next Room"))
+        {
+            manager.GenerateRoom();
+        }
+    }
+}
