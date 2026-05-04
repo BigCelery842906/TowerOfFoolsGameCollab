feaae134ffc09dc3abca86f02a5f8a7b9f28c5f0
xP9nda <c018182o@student.staffs.ac.uk>
2026-05-04 21:10:00 +0100

    Update ui_CreditsMenuManager.cs

--- Assets/Scripts/UI/ui_CreditsMenuManager.cs
+++ Assets/Scripts/UI/ui_CreditsMenuManager.cs
@@ -17,11 +17,9 @@ public class ui_CreditsMenuManager : ui_BaseMenuManager
     
     private void HandleButtonClicked_Quit()
     {
-        // Only allow the play button to be clicked once
         if (m_quitButtonClicked) return;
         m_quitButtonClicked = true;
         
-        // Load the scene via build index if that option is selected, if not, load via scene name
         HideMenu();
         m_mainMenuManager.ShowMenu();
 
