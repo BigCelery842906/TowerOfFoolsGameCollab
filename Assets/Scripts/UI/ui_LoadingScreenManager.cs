7c7b05ba03588e700c65e15e4c0aa2b548e50988
xP9nda <93463780+xP9nda@users.noreply.github.com>
2026-05-09 15:14:49 +0100

    concept art on loading screen

--- Assets/Scripts/UI/ui_LoadingScreenManager.cs
+++ Assets/Scripts/UI/ui_LoadingScreenManager.cs
@@ -1,12 +1,21 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using UI;
+using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ui_LoadingScreenManager : ui_BaseMenuManager
 {
+
+    [SerializeField] private List<Texture2D> m_conceptArtAssets;
+    
     protected override void InitialiseMenuManager()
     {
-        return;
+        VisualElement conceptArt = m_uiDocument.rootVisualElement.Q<VisualElement>("concept-art");
+        if (conceptArt == null) return;
+
+        conceptArt.style.backgroundImage = new StyleBackground(m_conceptArtAssets[UnityEngine.Random.Range(0, m_conceptArtAssets.Count)]);
     }
 
     // public void Hide()
